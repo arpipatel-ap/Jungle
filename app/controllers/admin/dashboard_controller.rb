dc2ead824afcbec9a4be48036e0f04f8ef637467
arpipatel-ap <arpipatel96@gmail.com>
2024-06-29 03:01:44 -0400

    Admin Dashboard - Counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
   def show
+    Rails.logger.debug "Admin Username: #{ENV['ADMIN_USERNAME']}"
+    Rails.logger.debug "Admin Password: #{ENV['ADMIN_PASSWORD']}"
+    @products = Product.count
+    @categories = Category.count
   end
 end
